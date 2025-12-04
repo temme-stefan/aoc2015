5324fa0f1cc6cf5b6da9cf007f983329c36ee58f
Stefan Temme <temme@objectcode.de>
2025-12-04 10:16:06 +0100

    initial commit

---
+++ scripts/getData.ts
@@ -0,0 +1,43 @@
+import {JSDOM} from "jsdom";
+import 'dotenv/config';
+import process from "node:process";
+
+async function getResponse(url: string): Promise<Response | null> {
+    const token = process.env.SESSION_TOKEN;
+    if (!token) {
+        console.error(`Could not fetch ${url}. Token Missing.`)
+        return null
+    }
+    const response = await fetch(url, {
+        headers: {
+            Cookie: `session=${token}`
+        }
+    })
+    if (!response.ok) {
+        console.error(`Could not fetch ${url}. Error in response. ${response.status} - ${response.statusText}`);
+        return null
+    }
+    return response;
+}
+
+export async function getText(url: string): Promise<string> {
+    const response = await getResponse(url);
+    if (response) {
+        return await response.text();
+    }
+    return "";
+}
+
+export async function getJson<T = unknown>(url: string): Promise<T | null> {
+    const response = await getResponse(url);
+    if (response) {
+        return await response.json();
+    }
+    return null;
+}
+
+export async function getExample(url: string): Promise<string> {
+    const html = await getText(url);
+    const {document} = (new JSDOM(html)).window;
+    return [...document.querySelectorAll("pre>code")].filter(x => (x.parentElement?.previousElementSibling?.textContent ?? "").endsWith("example:"))[0]?.textContent ?? "";
+}
